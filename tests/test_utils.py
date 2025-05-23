e81b851c7592385f19066b23e253a84a4fae1806
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-05-23 11:12:38 +0000

    test: replace assertEquals by assertEqual

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,7 +9,7 @@ class Test(TestCase):
     def test_execute1(self):
         cmd = "cronwrap --lock --load-env --low --timeout=10 sleep 1"
         output = subprocess.check_output(cmd, shell=True)
-        self.assertEquals(output, b"")
+        self.assertEqual(output, b"")
 
     def test_execute2(self):
         cmd = "cronwrap --lock --load-env --low --timeout=1 sleep 2"
@@ -17,5 +17,5 @@ class Test(TestCase):
             subprocess.check_output(cmd, shell=True)
             raise Exception("CalledProcessError not raised")
         except subprocess.CalledProcessError as e:
-            self.assertEquals(e.returncode, 12)
+            self.assertEqual(e.returncode, 12)
             return
